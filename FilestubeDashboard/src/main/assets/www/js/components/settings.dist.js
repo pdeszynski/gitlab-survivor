642055c4b2c88b38ad64a22b3a073e09773fd2da
Piotr Deszynski <deszynski@red-sky.pl>
2014-10-13 22:48:06 +0200

    Groupped settings by backend

--- FilestubeDashboard/src/main/assets/www/js/components/settings.dist.js
+++ FilestubeDashboard/src/main/assets/www/js/components/settings.dist.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
     angular.module('ftDashboard.components.settings', [])
+        .value('version', '0.1')
+
         .value('youtrackUri', 'Your YoutrackUri with trailing slash')
         .value('youtrackLogin', 'your youtrack login with admin rights')
         .value('youtrackPassword', 'your password')
@@ -8,23 +10,22 @@
         .value('youtrackUserRole', 'youtrack user role to be tracked in hall')
         .value('youtrackAgileBoard', 'name of the agile board')
 
-        .value('version', '0.1')
         .value('redmineKey', 'your redmine key')
-        .value('gitlabToken', 'your gitlab private token')
-        .value('jenkinsToken', 'your jenkins private token')
         .value('redmineUri', 'your redmine URI with trailing slash')
-        .value('hallLength', 3)
-        //defines how long pagination should be done before quitting
-        .value('maxRecursiveCalls', 200)
         .factory('projectUri', ['redmineUri', function(redmineUri) {
              return redmineUri + '/projects/[your project name in a redmine]/';
         }])
+        //redmine group id
         .value('groupId', 19)
+
         .value('gitlabProjectId', 953)
+        .value('gitlabToken', 'your gitlab private token')
         .value('gitlabUri', 'your gitlab URI with a trailing slash')
         .factory('gitlabProjectUri', ['gitlabUri', 'gitlabProjectId', function(gitlabUri, gitlabProjectId) {
             return gitlabUri + 'api/v3/projects/' + gitlabProjectId + '/';
         }])
+
+        .value('jenkinsToken', 'your jenkins private token')
         .value('jenkinsUri', 'Your jenkins URI with a trailing slash')
         .factory('jenkinsProjectUri', ['jenkinsUri', function(jenkinsUri) {
             return jenkinsUri + 'job/<your project name>/lastBuild/api/json';
@@ -33,5 +34,9 @@
         .factory('Versions', ['$resource', 'projectUri', 'redmineKey', function ($resource, projectUri, redmineKey) {
             return $resource(projectUri + 'versions.json', {key: redmineKey});
         }])
+
+        .value('hallLength', 3)
+        //defines how long pagination should be done before quitting
+        .value('maxRecursiveCalls', 200)
         ;
 })();
